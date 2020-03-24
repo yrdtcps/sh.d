f937478fdcdaa09a68b08e4eddf21fed48aed164
TCPS <you@example.com>
2020-03-24 14:23:19 +0800

    rssh get remote ip every 5 minites,if ip changed restart new autossh.

--- rssh/rssh.sh
+++ rssh/rssh.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
-REMOTE_IP=`curl -s https://blog.csdn.net/yrdtjtk/article/details/105064258 | grep -Eo 'content="218.68.[0-9.]+"' | grep -Eo '[0-9.]+'`
-if [ -z "$REMOTE_IP" ]; then
-    echo Obtain Remote IP error!
-    exit 1
-fi
-echo Remote IP: $REMOTE_IP
 
-#ssh -NfR 2021:localhost:22 root@$REMOTE_IP -p2020
-autossh -M 2022 -NR 2021:localhost:22 root@$REMOTE_IP -p2020 &
+getRemoteIP(){
+    local remote_ip=`curl -s https://blog.csdn.net/yrdtjtk/article/details/105064258 | grep -Eo '<p>218.68.[0-9.]+' | grep -Eo '[0-9.]+'`
+    echo $remote_ip
+}
+
+getRunningTaskPID(){
+    local ptask="`ps aux | grep 'autossh -M 2022' | grep -v 'grep'`"
+    if [ -z "$ptask" ]; then
+        echo No task need to be killed. >&2
+        exit 1
+    fi
+    #echo Current running task is:$ptask >&2
+    local pidtask=`echo $ptask | awk '{print $2}'`
+    #echo Current running task pid is $pidtask
+    echo $pidtask
+    exit 0
+}
+
+killRunningTask(){
+    pidtask=`getRunningTaskPID`
+    if [ $? -eq 0 -a -n "$pidtask" ]; then
+        echo Kill the current running task whose pid is $pidtask
+        kill $pidtask
+    fi
+}
+
+OLD_IP=
+while :; do
+    REMOTE_IP=`getRemoteIP`
+    if [ -z "$REMOTE_IP" ]; then
+        echo Obtain Remote IP error!
+        #exit 1
+    elif [ "$OLD_IP" != "$REMOTE_IP" ]; then
+        echo Remote IP: $REMOTE_IP
+        OLD_IP=$REMOTE_IP
+        killRunningTask
+        #ssh -NfR 2021:localhost:22 root@$REMOTE_IP -p2020
+        autossh -M 2022 -NR 2021:localhost:22 root@$REMOTE_IP -p2020 &
+    fi
+    #echo ------Remote IP: $REMOTE_IP
+    sleep 300
+done
 
 exit 0
